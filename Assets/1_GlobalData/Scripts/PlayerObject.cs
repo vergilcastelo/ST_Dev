08d5916b81a4ac4b6bd17d91da7e284761fc2a28
vergilcastelo <theworldeater@gmail.com>
2023-12-24 01:43:41 -0800

    add public data and generate method
    - name and color vars added
    - created generate method
    - added name creation code to set public name

--- Assets/1_GlobalData/Scripts/PlayerObject.cs
+++ Assets/1_GlobalData/Scripts/PlayerObject.cs
@@ -2,17 +2,34 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/*
+Player Object
+generates it own public data Name and Color
+data based upon lists in GlobalDataManager
+renames itself to lowercase Name data
+*/
 public class PlayerObject : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    //Name var proper case, unique string from AdjectivesList and ItemNamesList
+    public string Name;
+
+    //Color var chosen from ColorsList 
+    public Color Color;
+
+    //Generate method used to populate our player data in editor only    
+     public void Generate()
     {
+        //local vars taken from AdjectivesList and ItemNamesList randomly
+        string adjective = GlobalDataManager.AdjectivesList[Random.Range(0, GlobalDataManager.AdjectivesList.Count)];
+        string itemName = GlobalDataManager.ItemNamesList[Random.Range(0, GlobalDataManager.ItemNamesList.Count)];
         
+        //Set Name to combination of local vars
+        Name = adjective + " " + itemName;
+
+        //log it to check 
+        Debug.Log(Name);
+
+        //Create Color 
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
