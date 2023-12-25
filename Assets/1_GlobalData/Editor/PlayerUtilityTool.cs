687445efc56ccd984db8fec7e674768336f6ce03
vergilcastelo <theworldeater@gmail.com>
2023-12-24 18:24:24 -0800

    create Editor tool
    - created Editor folder
    - created PlayerUtilityTool script
    - added script to Editor folder
    - import namespaces
    
    - created GameManager prefab

---
+++ Assets/1_GlobalData/Editor/PlayerUtilityTool.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using System.Linq;
+
+public class PlayerUtilityTool : EditorWindow
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
