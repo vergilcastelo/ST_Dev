1125d3ee0242659bc8f3fcf80905439b9f5e6075
vergilcastelo <theworldeater@gmail.com>
2023-12-24 18:39:58 -0800

    Update PlayerUtilityTool.cs
    - add scrolling, search, and list variables
    - create editor window
    - window accessible from top window menu

--- Assets/1_GlobalData/Editor/PlayerUtilityTool.cs
+++ Assets/1_GlobalData/Editor/PlayerUtilityTool.cs
@@ -6,6 +6,25 @@ using System.Linq;
 
 public class PlayerUtilityTool : EditorWindow
 {
+    //track scroll position in scrolling window
+    private Vector2 scrollPosition;
+
+    //search query string
+    private string searchQuery = "";
+
+    //list for filtering player objects
+    private List<PlayerObject> filteredPlayers = new List<PlayerObject>();
+
+    //create tool accessible from the top menu under window
+    [MenuItem("Window/Player Utility Tool")]
+
+    //show sindow method triggered by menuitem above
+    public static void ShowWindow()
+    {
+        //create editor window of this class 
+        GetWindow<PlayerUtilityTool>("Player Utility Tool");
+    }
+
     // Start is called before the first frame update
     void Start()
     {
