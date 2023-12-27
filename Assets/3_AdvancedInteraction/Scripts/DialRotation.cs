e1b07bc7f570c0a3a0da0fb0cc563f6f7d850931
vergilcastelo <theworldeater@gmail.com>
2023-12-27 08:51:55 -0800

    update DialRotation script
    - add comments to explain code and processes

--- Assets/3_AdvancedInteraction/Scripts/DialRotation.cs
+++ Assets/3_AdvancedInteraction/Scripts/DialRotation.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class DialRotation : MonoBehaviour
 {
+    //rotationspeed how fast the dial turns when mouse moves
      private float rotationSpeed = 100f;
     private Vector3 lastMousePosition;
     // Start is called before the first frame update
@@ -15,17 +16,22 @@ public class DialRotation : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-          if (Input.GetMouseButtonDown(0))
+        //rotation 
+        //when left mouse button is released 
+        if (Input.GetMouseButtonDown(0))
         {
+            //track mouse position
             lastMousePosition = Input.mousePosition;
         }
-
+        //when left mouse button 0 is held down
         if (Input.GetMouseButton(0))
         {
+            //delta difference between last position and current
             Vector3 delta = Input.mousePosition - lastMousePosition;
             lastMousePosition = Input.mousePosition;
-
+            //angle = rotation in degrees (diff x how fast we rotate x how long rotation took)
             float angle = delta.x * rotationSpeed * Time.deltaTime;
+            //rotat dial according to angle
             transform.Rotate(Vector3.forward, angle, Space.World);
         }
     }
